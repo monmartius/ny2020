1c96fa484df70f801b6aa802f98de104f5c9e8e7
monmartius <monmartius@gmail.com>
2019-12-11 19:36:05 +0700

    something also...

--- step6.php
+++ step6.php
@@ -5,11 +5,7 @@
             <div class="col-12 col-lg-8 offset-lg-2">
             	<h2 class="results-h2">Поздравляем!</h2>
 	            <div class="congratulations">
-	            Вы прошли тест от&nbsp;справочной по&nbsp;ценам и&nbsp;скидкам 2999999 о&nbsp;готовности к&nbsp;Новому году и&nbsp;получили подарок – бесплатное размещение баннера на&nbsp;<a href="https://2999999.ru">сайте 2999999.ru</a>.</div>
-
-
-				<div class="congratulations-v-gutter"></div>
-	            <div class="congratulations text-right">До встречи в&nbsp;Новом году!</div>
+	            <p>Вы прошли тест от&nbsp;справочной по&nbsp;ценам и&nbsp;скидкам 2999999 о&nbsp;готовности к&nbsp;Новому году и&nbsp;получили подарок – бесплатное размещение баннера на&nbsp;<a class = "link-new" href="https://2999999.ru">сайте 2999999.ru</a>.</p><p style="text-align: right; padding-bottom: 0; margin-bottom: 0">До встречи в&nbsp;Новом году!</p></div>
             </div>
 
         </div><!-- row -->
